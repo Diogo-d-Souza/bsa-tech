dd30bce8400a37b8892da73b27b40adba7795265
Diogo-d-Souza <di_smgomes@hotmail.com>
2025-04-29 15:29:04 -0300

    feat: initial project with websocket setup

---
+++ websocket/consumer.py
@@ -0,0 +1,9 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class BasicConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.send(text_data=json.dumps({
+            'type': 'connected',
+        }))
